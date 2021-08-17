0aaed89e1f58f8469e5c7e0a4015c145c58063e1
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-08-17 08:56:40 +0530

    Switch Case

---
+++ Switch.java
@@ -0,0 +1,30 @@
+package myproject;
+
+public class Switch {
+	public static void main(String[] args) {
+		//Switch
+		int day = 3;
+		switch(day) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		default:
+			System.out.println("Saturday");
+		}
+	}
+}
