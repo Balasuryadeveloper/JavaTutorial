00fa6c4b14f5df1d3a85a407556d0f71de872300
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-08-17 08:27:09 +0530

    JavaBoolean file added

---
+++ Boolean.java
@@ -0,0 +1,16 @@
+package myproject;
+
+public class Boolean {
+	public static void main(String[] args) {
+		//Boolean
+		boolean myBool = true;
+		boolean myBool1 = false;
+		System.out.println(myBool);
+		System.out.println(myBool1);
+		System.out.println(10 > 12); // false
+		System.out.println(10 >= 12); // false
+		System.out.println(10 < 12); // true
+		System.out.println(10 <= 12); // true
+		System.out.println(10 != 12); // true
+	}
+}
