045dd94ce70fa919f534fcd9ca0a1a3b8d33c0c4
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-08-17 09:09:51 +0530

    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package myproject;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		//For loop
+		//Syntax
+		//		for (statement 1; statement 2; statement 3) {
+		//		  // code block to be executed
+		//		}
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+	}
+}
