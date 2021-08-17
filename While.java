993b250d5b91558f8b94e523b77a26444dbade8f
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-08-17 09:04:50 +0530

    while and do while loops

---
+++ While.java
@@ -0,0 +1,29 @@
+package myproject;
+
+public class While {
+	public static void main(String[] args) {
+		//While Loop
+		//		Syntax
+		//		while (condition) {
+		//		  // code block to be executed
+		//		}
+		
+		int i = 1;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		//do while
+		//Syntax
+		//		do {
+		//			  // code block to be executed
+		//			}
+		//			while (condition);
+		
+		int j = 1;
+		do {
+			System.out.println(j);
+			j++;
+		}while(j < 6);
+	}
+}
