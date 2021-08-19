09dd25b3514d06c867a8e7f6797e4d80308ae8e2
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-08-19 08:09:54 +0530

    create class and obj concept

---
+++ Oops1.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class Oops1 {
+	//create class
+	int x =10;
+	public static void main(String[] args) {
+		//create an object
+		Oops1 obj = new Oops1();
+		System.out.println(obj.x);
+	}
+}
